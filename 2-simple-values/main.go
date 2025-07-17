a364c1a52f04833a42924a0aa7af6a27ff2589ff
Subhajit Chaudhury <subhajitchaudhury05@gmail.com>
2025-07-17 05:47:09 +0530

    added simple values

---
+++ 2-simple-values/main.go
@@ -0,0 +1,23 @@
+package main
+
+func main() {
+
+	// string
+	println("Subh is awesome!")
+
+	// integer
+	println(42)
+
+	// float
+	println(3.14)
+
+	// boolean
+	println(true)
+
+	// character
+	println('a')
+
+	// complex number
+	println(complex(1, 2))
+
+}
